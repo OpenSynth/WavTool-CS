d6e4f3562a5e5939081d4400341d3cf8870a179a
740291272 <740291272@qq.com>
2018-08-12 15:54:26 +0800

    first commit

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace VocalUtau.WavTools
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArgsStruct p=ArgsParser.parseArgs(args);
+            if (p == null)
+            {
+                ArgsParser.printUsage();
+                return;
+            }
+
+            WavTool_Prg wtool = new WavTool_Prg(p.Outputfilename);
+            wtool.WavTool_Init();
+
+            for (int w = 0; w < 5; w++)
+            {
+                ArgsParser.printArgs(p);
+                wtool.WavTool_Append(p);
+            }
+
+            wtool.WavTool_Close();
+            return;
+        }
+    }
+}
---
+++ WavTool_Prg.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace VocalUtau.WavTools
+{
+    class WavTool_Prg
+    {
+        string outfile_wavhdr;
+        string outfile_wavdat;
+        long MaxLength = 0;
+        public WavTool_Prg(string Outputfilename)
+        {
+           outfile_wavhdr = Outputfilename + ".whd";
+           outfile_wavdat = Outputfilename + ".dat";
+           MaxLength = 0;
+        }
+
+        public void WavTool_Init()
+        {
+            if (!System.IO.File.Exists(outfile_wavhdr))
+            {
+                WavFile_Heads.wfh_init(outfile_wavhdr);
+            }
+            if (!System.IO.File.Exists(outfile_wavdat))
+            {
+                WavFile_Datas.wfd_init(outfile_wavdat);
+            }
+        }
+
+        public void WavTool_Append(ArgsStruct p)
+        {
+            int len = 0;
+            len = WavFile_Datas.wfd_append(outfile_wavdat, p.Inputfilename, p.Offset, p.Length, p.Ovr, p.PV);
+            MaxLength += len;
+        }
+
+        public void WavTool_Close()
+        {
+            int len = (int)MaxLength;
+            if (WavFile_Heads.wfh_checkIslegal(outfile_wavhdr))
+            {
+                int result = WavFile_Heads.wfh_putlength(outfile_wavhdr, len);
+            }
+        }
+    }
+}
